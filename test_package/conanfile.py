ae1fdd3cae66dfd513ec502db713a6548a2c63e4
yjjnls <x-jj@foxmail.com>
2018-11-28 09:46:44 +0800

    update test_package settings

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -9,6 +9,12 @@ class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
 
+    def is_emscripten(self):
+        try:
+            return self.settings.compiler == 'emcc'
+        except:
+            return False
+
     def configure(self):
         if self.settings.compiler == 'gcc' and str(self.settings.compiler.version) >= '5':
             self.settings.compiler.libcxx = 'libstdc++11'
@@ -21,7 +27,9 @@ class TestPackageConan(ConanFile):
     def test(self):
         with tools.environment_append(RunEnvironment(self).vars):
             bin_path = os.path.join("bin", "test_package")
-            if self.settings.os == "Windows":
+            if self.is_emscripten():
+                self.run("node %s.js"%bin_path)
+            elif self.settings.os == "Windows":
                 self.run(bin_path)
             elif self.settings.os == "Macos":
                 self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ.get('DYLD_LIBRARY_PATH', ''), bin_path))
